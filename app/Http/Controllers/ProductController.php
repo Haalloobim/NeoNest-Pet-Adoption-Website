33aff229030d835b5d1e561f809fd3ae0ec61bfa
jjchoNC <5025221001@student.its.ac.id>
2024-11-07 08:33:59 +0700

    feat: add pet detection

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,29 +4,51 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
-{   
+{
+    public function detect(UploadedFile $image)
+    {   
+        $path = $image->store('products', 'public');
+        $data = base64_encode(Storage::disk('public')->get($path));
+        $api_key = env('ROBOFLOW_API_KEY');
+        $model_endpoint = 'oxford-pets-tgv53/1';
+
+        $url = "https://detect.roboflow.com/" . $model_endpoint
+            . "?api_key=" . $api_key
+            . "&name=" . $image->hashName();
+
+        $options = array(
+            'http' => array(
+                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => $data
+            )
+        );
+
+        $context  = stream_context_create($options);
+        $result = json_decode(file_get_contents($url, false, $context), true);
+        dd($result);
+    }
+
     public function upload(Request $request)
     {
         $image = $request->file('image');
-        $image->storeAs('public/images/products', $image->hashName());
-
-        if ($request->category == 'other') {
-            $category = $request->other_category;
-        } else {
-            $category = $request->category;
-        }
-
+        $cat = $this->detect($image);
+        dd($cat);
         Product::create([
             'name' => $request->name,
             'price' => $request->price,
             'description' => $request->description,
-            'category' => $category,
+            'category' => $request->category,
             'image' => $image->hashName(),
             'seller_id' => Auth::id(),
         ]);
+
+        
         return redirect()->route('seller.dashboard')->with('success', 'Product uploaded successfully!');
     }
 }
