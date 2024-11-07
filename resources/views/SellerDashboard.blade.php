dcdaeafd7562afd976833227abae991559087439
yaudahbanh <juniorvincent32@gmail.com>
2024-11-07 08:41:50 +0700

    feat: change title upload page

--- resources/views/SellerDashboard.blade.php
+++ resources/views/SellerDashboard.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload Product</title>
+    <title>Upload Pet</title>
     @vite('resources/css/app.css')
 </head>
 
